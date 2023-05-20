8baea4f21e90fdb5741f9dd9bdb310c44e63f049
Israel Mendoza <israelmendoza@192.168.1.6>
2023-05-20 14:23:29 -0600

    Done

---
+++ Consumo de electricidad CFE/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+var mensajeValorInicial = "Introduce el valor inicial de kWH consumidos en el mes: ";
+var mensajeValorFinal = "Introduce el valor de kWh consumidos hasta hoy: ";
+var mensajeFecha = "Intrudoce la fecha inicial de conteo en formato DD/MM/AA: ";
+
+decimal ObtenerNumeroValido(string mensaje, decimal valorAComprarar)
+{
+    decimal numeroValido;
+    string? input;
+    while (true)
+    {
+        Console.Write(mensaje);
+        input = Console.ReadLine();
+        var exitoEnConversion = decimal.TryParse(input, out numeroValido);
+        if (exitoEnConversion)
+        {
+            if (numeroValido >= valorAComprarar)
+            {
+                return numeroValido;
+            }
+            else
+            {
+                Console.WriteLine($"'{numeroValido}' no es un consumo válido. Debe ser mayor a '{valorAComprarar}'. Intente de nuevo.\n");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"'{input}' no es un valor válido. Intente de nuevo.\n");
+        }
+    }
+}
+
+DateTime ObtenerFechaValida(string mensaje)
+{
+    DateTime fechaHoraValida;
+    string? input;
+    while (true)
+    {
+        Console.Write(mensaje);
+        input = Console.ReadLine();
+        var exitoEnConversion = DateTime.TryParse(input, new CultureInfo("es-MX"), out fechaHoraValida);
+        if (exitoEnConversion)
+        {
+            return fechaHoraValida;
+        }
+        else
+        {
+            Console.WriteLine($"{input} no es un formato válido de fecha. Intente de nuevo.\n");
+        }
+    }
+}
+
+Console.Clear();
+
+var valorConsumoInicial = ObtenerNumeroValido(mensajeValorInicial, 0);
+var valorConsumoFinal = ObtenerNumeroValido(mensajeValorFinal, valorConsumoInicial);
+var fechaInicial = ObtenerFechaValida(mensajeFecha);
+
+var miRecibo = new Recibo(valorConsumoFinal - valorConsumoInicial, fechaInicial);
+
+Console.WriteLine();
+miRecibo.ImprimirInfo();
+
+var futuroRecibo = new Recibo(miRecibo.PromedioConsumoPorDia * 61, fechaInicial);
+Console.WriteLine($"\nPosible futuro recibo: \n");
+futuroRecibo.ImprimirInfo();
---
+++ Consumo de electricidad CFE/Recibo.cs
@@ -0,0 +1,82 @@
+public class Recibo
+{
+    public static decimal PrecioDAC = 6.98M;
+    public static decimal PrecioExcedente = 3.52M;
+    public static decimal PrecioIntermedio = 1.19M;
+    public static decimal PrecioBasico = 0.98M;
+    public static decimal IVA = 1.16M;
+    public decimal ConsumoTotal { get; init; }
+    decimal basico;
+    decimal intermedio;
+    decimal excedente;
+    decimal DAC;
+    public DateTime FechaInicial { get; init; }
+    public int Dias { get; init; }
+    public decimal PrecioFinal { get; init; }
+    public decimal PromedioConsumoPorDia { get; init; }
+    public Recibo(decimal consumoTotal, DateTime fechaInicial)
+    {
+        this.ConsumoTotal = consumoTotal;
+        // Inicializando los valores de las diferentes tarifas
+        if (this.ConsumoTotal >= 500)
+        {
+            basico = 0;
+            intermedio = 0;
+            excedente = 0;
+            DAC = this.ConsumoTotal;
+        }
+        else if (this.ConsumoTotal > 280)
+        {
+            DAC = 0;
+            excedente = this.ConsumoTotal - 280;
+            intermedio = 130;
+            basico = 150;
+        }
+        else if (this.ConsumoTotal > 150)
+        {
+            DAC = 0;
+            excedente = 0;
+            intermedio = this.ConsumoTotal - 150;
+            basico = 150;
+        }
+        else
+        {
+            DAC = 0;
+            excedente = 0;
+            intermedio = 0;
+            basico = this.ConsumoTotal;
+        }
+        // Calculando el precio final.
+        if (DAC != 0)
+        {
+            PrecioFinal = (DAC * PrecioDAC) * IVA;
+        }
+        else
+        {
+            PrecioFinal = ((basico * PrecioBasico) + (intermedio * PrecioIntermedio) + (excedente * PrecioExcedente) * IVA);
+        }
+        // Inicializando los últimos valores.
+        FechaInicial = fechaInicial;
+        Dias = (DateTime.Today - FechaInicial).Days;
+        PromedioConsumoPorDia = this.ConsumoTotal / (decimal)Dias;
+    }
+    public void ImprimirInfo()
+    {
+        var delimitador = new string('*', 50);
+        Console.WriteLine(delimitador);
+        Console.WriteLine($"Consumo de {ConsumoTotal}kWh en {Dias} días. Precio final: {this.PrecioFinal:C2}");
+        if (DAC != 0)
+        {
+            Console.WriteLine($"\tDAC: {DAC}");
+        }
+        else
+        {
+            Console.WriteLine($"");
+            Console.WriteLine($"\tBásico:\t\t{basico}kWh");
+            Console.WriteLine($"\tIntermedio:\t{intermedio}kWh");
+            Console.WriteLine($"\tExcedente:\t{excedente}kWh");
+        }
+        Console.WriteLine($"Consumo diario promedio: {PromedioConsumoPorDia:F2}kWh");
+        Console.WriteLine(delimitador);
+    }
+}
